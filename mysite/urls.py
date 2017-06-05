a130334935e4aca254cd4c36947a5200bb0c439a
Cuiqing Li <lixx3527@gmail.com>
2017-06-04 23:13:54 -0400

    create views

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
 ]
